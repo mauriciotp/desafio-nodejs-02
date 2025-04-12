c8541ec76930662eba14c87ec265f1997ad30753
mauricio <mauricio.tp_@outlook.com>
2025-04-12 11:43:21 -0300

    feat: validate env variables

---
+++ src/env.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod'
+
+const envSchema = z.object({
+  DATABASE_URL: z.string(),
+  PORT: z.number().default(3333),
+})
+
+const parsedEnv = envSchema.safeParse(envSchema)
+
+if (!parsedEnv.success) {
+  console.error(
+    'Invalid environment variables!',
+    parsedEnv.error.flatten().fieldErrors
+  )
+  throw new Error('Invalid environment variables!')
+}
+
+export const env = parsedEnv.data
